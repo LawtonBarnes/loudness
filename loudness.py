3a07d7bb66b80e74456e823a7dc88065d1868a08
Lawton Barnes <lawtonbarnes@gmail.com>
2026-08-15 05:08:50 -0500

    Bump VERSION to 1.2 -- end of session checkpoint

--- loudness.py
+++ loudness.py
@@ -30,7 +30,7 @@ os.environ.setdefault("SDL_VIDEODRIVER", "dummy")
 
 import pygame  # noqa: E402  (must come after SDL env vars are set)
 
-VERSION = "1.1"
+VERSION = "1.2"
 
 BASE_DIR = Path(__file__).resolve().parent
 SETTINGS_PATH = BASE_DIR / "settings.ini"
